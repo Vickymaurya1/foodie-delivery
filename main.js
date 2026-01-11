3577e66d6157a853a903fe75d22aaf6e9726b509
Vickymaurya1 <vickymaurya2005@gmail.com>
2026-01-12 00:35:54 +0530

    cart update

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: "#prev",
       },
     });
+
+    
+const cartIcon = document.querySelector('.cart-icon');
+const cartTab = document.querySelector('.cart-tab');
+const closeBtn = document.querySelector('.close-btn');
+
+cartIcon.addEventListener('click', ()=> cartTab.classList.add('cart-tab-active'));
+closeBtn.addEventListener('click', ()=> cartTab.classList.remove('cart-tab-active'));
+
